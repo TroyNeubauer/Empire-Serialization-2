564ee0ddb773a8e18ebe23e6a68415c60fe77b50
Troy Neubauer <troyneubauer@gmail.com>
2019-01-30 10:26:36 -0500

    Created u128 addition test

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
+
 #include "Buffer.h"
 #include "EmpireException.h"
 #include "EmpireErrorCodes.h"
+#include "EmpirePrimitives.h"
 
 using namespace Empire;
 
-int test(int x ERROR_CODE_PARAMETER) {
-	if (x == 5) {
-		ERROR(EMPIRE_INVALID_ARGUMENT,0)
-	}
-	return 0;
-}
 
 
 int main() {
-	ErrorCode code = 0;
-	try {
-		test(15, code);
-	} catch (std::exception e) {
-		std::cout << "Error! Code " << code << " : " << e.what() << std::endl;
-	}
-	try {
-		test(5, code);
-	} catch (std::exception e) {
-		std::cout << "Error! Code " << code << " : " << e.what() << std::endl;
-	}
+	u128 test(0x0011223344556677, 0x8899AABBCCDDEEFF);
+	std::cout << "test: " << test << std::endl << std::endl;
+	u128 value = 0xFFFFFFFFFFFFFF;
+	u128 value2 = 0xFFFFFFFFFFFFFF;
+	std::cout << value << std::endl;
+	std::cout << value2 << std::endl;
+	std::cout << "+ ________________" << std::endl;
+	u128 result = value + value2;
+	std::cout << result << std::endl;
+
 	system("PAUSE");
 	Buffer buffer;
-	buffer.ensureCapacity(12);
+	buffer.EnsureCapacity(12);
 
 
 	FILE* file = fopen("data.dat", "wb");
-	if (file != NULL) {
-		fwrite(buffer.getStart(), 1, buffer.offset(), file);
+	if (file) {
+		fwrite(buffer.GetStart(), 1, buffer.Offset(), file);
+		fclose(file);
 	}
-	fclose(file);
 }
