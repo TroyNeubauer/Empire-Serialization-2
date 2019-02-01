b9fb3348c717c82dcaad7421a7c360bbffff6495
Troy Neubauer <troyneubauer@gmail.com>
2019-01-31 22:30:29 -0500

    Added endianess check

---
+++ C++/Empire.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "EmpireEndianness.h"
+#include "Buffer.h"
+#include "EmpireErrorCodes.h"
+#include "EmpireException.h"
+#include "EmpirePrimitives.h"
+#include "TypeManager.h"
+
+
--- C++/EmpirePrimitives.h
+++ C++/EmpirePrimitives.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "Empire.h"
+
 #include <stdint.h>
+#include <stdlib.h>
 #include <iostream>
 #include <ios>
 #include <string>
@@ -12,9 +15,7 @@
 #include <wmmintrin.h>
 
 
-
 #define EMPIRE_ENABLE_INTEL_INTRINSICS 1
-
 
 #if !EMPIRE_ENABLE_INTEL_INTRINSICS
 #error Cannot compile without intrinsics! intrinsics needed for addition and subtraction
@@ -37,54 +38,61 @@ typedef __int128 s128;
 typedef unsigned __int128 u128;
 
 #else
-
-union S {
-	__m128i m128i;
-	struct {
-		u64 high, low;
-	};
-};
+namespace Empire {
 
 
 union u128 {
-#ifdef _INCLUDED_EMM // __m128i is inside of this header file, so we know __m128i exists if the file is included
-	#define EMPIRE_M128_UNION
-	__m128i m128i;
-#endif
-	struct {
-		u64 high, low;
-	};
-	u32 u128_u32[4];
+	u64 _64[2];
+	u32 _32[4];
+	u16 _16[8];
+	u8  _8[16];
 
 	u128() {}
-	u128(const u64 value) : high(0), low(value) {}
-	u128(const u64 high, u64 low) : high(high), low(low) {}
-	u128(const u128& value) : high(value.high), low(value.low) {}
-	u128(const __m128i& value) : high(value.m128i_u64[0]), low(value.m128i_u64[1]) {}
+	u128(const u64 value) {
+		u128* t = this;
+		if (SYSTEM_ENDIANESS == Endianness::LITTLE) {
+			this->_64[0] = value;
+		} else {
+			this->_64[1] = value;
+		}
+	}
 
-	const __m128i To__m128i() {
-		__m128i result;
-		result.m128i_u64[0] = this->high;
-		result.m128i_u64[1] = this->low;
-		return result;
+	u128(const u64 high, u64 low) {
+		u128* t = this;
+		if (SYSTEM_ENDIANESS == Endianness::LITTLE) {
+			this->_64[1] = high;
+			this->_64[0] = low;
+		} else {
+			this->_64[0] = high;
+			this->_64[1] = low;
+		}
+	}
+
+	u128(const u128& value) {
+		u128* t = this;
+		this->_64[0] = value._64[0];
+		this->_64[1] = value._64[1];
 	}
 
 	const bool operator==(const u128 other) {
-		return this->high == other.high && this->low == other.low;
+		return this->_64[0] == other._64[0] && this->_64[1] == other._64[1];
 	}
 	const bool operator!=(const u128 other) {
 		return !(*this == other);
 	}
 	u128 operator=(const u128 other) {
-		this->high = other.high;
-		this->low = other.low;
+		this->_64[0] = other._64[0];
+		this->_64[1] = other._64[1];
 		return *this;
 	}
+
+	//Little 00 11 22 33 44 55 66 77    88 99 AA BB CC DD EE FF - as 128 bit
+	//Big    FF EE DD CC BB AA 99 88    77 66 55 44 33 22 11 00 - ad 128 bit
 	void ShiftLeft(u64 bits, u128& result) {
-		result.high = this->high << bits;
-		u64 lowerOfUpper = this->low >> (sizeof(u64) * 8 - bits);
-		result.high |= lowerOfUpper;
-		result.low = this->low << bits;
+		result._64[0] = this->_64[0] << bits;
+		u64 lowerOfUpper = this->_64[1] >> (sizeof(u64) * 8 - bits);
+		result._64[0] |= lowerOfUpper;
+		result._64[1] = this->_64[1] << bits;
 	}
 	u128 operator<<(u64 bits) {
 		u128 result;
@@ -97,10 +105,10 @@ union u128 {
 	}
 
 	void ShiftRight(u64 bits, u128& result) {
-		result.low = this->low >> bits;
-		u64 upperOfLower = this->high << (sizeof(u64) * 8 - bits);
-		result.low |= upperOfLower;
-		result.high = this->high >> bits;
+		result._64[1] = this->_64[1] >> bits;
+		u64 upperOfLower = this->_64[0] << (sizeof(u64) * 8 - bits);
+		result._64[1] |= upperOfLower;
+		result._64[0] = this->_64[0] >> bits;
 	}
 	u128 operator>>(u64 bits) {
 		u128 result;
@@ -111,11 +119,11 @@ union u128 {
 		ShiftRight(bits, *this);
 		return *this;
 	}
-	
+
 	void Plus(const u128& other, u128& result) {
 #if EMPIRE_ENABLE_INTEL_INTRINSICS
-		char carry = _addcarry_u64(0, this->low, other.low, &result.low);// Add low bits
-		_addcarry_u64(carry, this->high, other.high, &result.high);// Add high bits with the carry from the low bits
+		char carry = _addcarry_u64(0, this->_64[1], other._64[1], &result._64[1]);// Add low bits
+		_addcarry_u64(carry, this->_64[0], other._64[0], &result._64[0]);// Add high bits with the carry from the low bits
 #endif
 	}
 	const u128 operator+(const u128 other) {
@@ -139,8 +147,8 @@ union u128 {
 
 	void Minus(const u128& other, u128& result) {
 #if EMPIRE_ENABLE_INTEL_INTRINSICS
-		char carry = _subborrow_u64(0, this->low, other.low, &result.low);// Add low bits
-		_subborrow_u64(carry, this->high, other.high, &result.high);// Add high bits with the carry from the low bits
+		char carry = _subborrow_u64(0, this->_64[1], other._64[1], &result._64[1]);// Add low bits
+		_subborrow_u64(carry, this->_64[0], other._64[0], &result._64[0]);// Add high bits with the carry from the low bits
 #endif
 	}
 
@@ -163,18 +171,26 @@ union u128 {
 		return temp;
 	}
 
-	inline u32 MutiplyWithCarry(u32 a, u32 b, u32& carry) {
-		u64 result = ((u64) a) * ((u64) b) + ((u64) carry);
-		carry = result >> 32;
-		return (u32) (result & 0xFFFFFF);
+	inline u32 MutiplyWithCarry(u32 a, u32 b, u64& carry) {
+		u64 total = ((u64)a) * ((u64)b) + ((u64)carry);
+		carry = total >> 32;
+		u32 result = (u32)total;
+		return result;
 	}
 
 	void Multiply(const u32& other, u128& result) {
-		u32 carry = 0;
-		this->u128_u32[3] = MutiplyWithCarry(this->u128_u32[3], other, carry);
-		this->u128_u32[2] = MutiplyWithCarry(this->u128_u32[2], other, carry);
-		this->u128_u32[1] = MutiplyWithCarry(this->u128_u32[1], other, carry);
-		this->u128_u32[0] = MutiplyWithCarry(this->u128_u32[0], other, carry);
+		u64 carry = 0;
+		if (SYSTEM_ENDIANESS == Endianness::LITTLE) {//Start from lower addresses since thats where the lower values are
+			this->_32[0] = MutiplyWithCarry(this->_32[0], other, carry);
+			this->_32[1] = MutiplyWithCarry(this->_32[1], other, carry);
+			this->_32[2] = MutiplyWithCarry(this->_32[2], other, carry);
+			this->_32[3] = MutiplyWithCarry(this->_32[3], other, carry);
+		} else {//Start from the higher addresses - we are BE
+			this->_32[3] = MutiplyWithCarry(this->_32[3], other, carry);
+			this->_32[2] = MutiplyWithCarry(this->_32[2], other, carry);
+			this->_32[1] = MutiplyWithCarry(this->_32[1], other, carry);
+			this->_32[0] = MutiplyWithCarry(this->_32[0], other, carry);
+		}
 	}
 
 	const u128 operator*(const u32 other) {
@@ -194,10 +210,10 @@ union u128 {
 	}
 	void Divide(const u32 other, u128& result) {
 		u32 remainder = 0;
-		this->u128_u32[0] = DivideWithRemainder(this->u128_u32[0], other, remainder);
-		this->u128_u32[1] = DivideWithRemainder(this->u128_u32[1], other, remainder);
-		this->u128_u32[2] = DivideWithRemainder(this->u128_u32[2], other, remainder);
-		this->u128_u32[3] = DivideWithRemainder(this->u128_u32[3], other, remainder);
+		this->_32[0] = DivideWithRemainder(this->_32[0], other, remainder);
+		this->_32[1] = DivideWithRemainder(this->_32[1], other, remainder);
+		this->_32[2] = DivideWithRemainder(this->_32[2], other, remainder);
+		this->_32[3] = DivideWithRemainder(this->_32[3], other, remainder);
 	}
 
 	const u128 operator/(const u32 other) {
@@ -218,58 +234,61 @@ struct s128 {
 
 #endif//#if defined(__SIZEOF_INT128__) && (__SIZEOF_INT128__ == 16)
 
-const char DIGITS[16] = {'0', '1', '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , 'A' , 'B' , 'C' , 'D' , 'E' , 'F' };
+const char DIGITS[16] = { '0', '1', '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , 'A' , 'B' , 'C' , 'D' , 'E' , 'F' };
 
 std::ostream& operator<<(std::ostream& os, const u128 value) noexcept
 {
 	if (os.flags() & std::ios_base::hex) {
-		char buffer[40];
-		int i = 0;
-		for (int byte = sizeof(u64) - 1; byte >= 0; byte--) {
-			buffer[i++] = DIGITS[(value.high >> (byte * 8 + 4)) & 0b1111];
-			buffer[i++] = DIGITS[(value.high >> (byte * 8 + 0)) & 0b1111];
-			if ((byte != 0) && (byte % 2) == 0) {
-				buffer[i++] = ' ';
+		char buffer[36];
+		int index = 0;
+		for (int i = 0; i < sizeof(u128); i++) {
+			char part;// We put the higher chars in the buffer first...
+			if (SYSTEM_ENDIANESS == Endianness::LITTLE)
+				part = value._8[sizeof(u128) - 1 - i];// The higher parts start at higher indices
+			else
+				part = value._8[i];// The higher parts start at lower indices
+			
+			buffer[index++] = DIGITS[(part >> 4) & 0b1111];
+			buffer[index++] = DIGITS[(part >> 0) & 0b1111];
+
+			if ((i != 0) && (i % (sizeof(u128) / 2)) == 0) {// Put an extra space half way through
+				buffer[index++] = ' ';
+			}
+			if ((i != 0) && (i % (sizeof(u128) / 4)) == 0) {// Put a space every 32 bits
+				buffer[index++] = ' ';
 			}
 		}
-		buffer[i++] = ' ';
-		buffer[i++] = ' ';
-		for (int byte = sizeof(u64) - 1; byte >= 0; byte--) {
-			buffer[i++] = DIGITS[(value.low >> (byte * 8 + 4)) & 0b1111];
-			buffer[i++] = DIGITS[(value.low >> (byte * 8 + 0)) & 0b1111];
-			if ((byte != 0) && (byte % 2) == 0) {
-				buffer[i++] = ' ';
-			}
-		}
-		assert(i == sizeof(buffer));
+		assert(index == sizeof(buffer));
 		os.write(buffer, sizeof(buffer));
-	} else if (os.flags() & std::ios_base::oct) {//Print binary instead of oct
-		char buffer[147];
-		int i = 0;
-		for (int bit = sizeof(u64)*8 - 1; bit >= 0; bit--) {
-			buffer[i++] = DIGITS[(value.high >> bit) & 0b1];
-			if ((bit != 0) && (bit % 8) == 0) {
-				buffer[i++] = ' ';
+	}
+	else if (os.flags() & std::ios_base::oct) {//Print binary instead of oct
+		char buffer[148];
+		int index = 0;
+		for (int i = 0; i < sizeof(u128); i++) {
+			char part;// We put the higher chars in the buffer first...
+			if (SYSTEM_ENDIANESS == Endianness::LITTLE) 
+				part = value._8[sizeof(u128) - 1 - i];// The higher parts start at higher indices
+			else 
+				part = value._8[i];// The higher parts start at lower indices
+			
+			for (int bit = 7; bit >= 0; bit--) {
+				buffer[index++] = DIGITS[(part >> bit) & 0b1];
 			}
-			if ((bit != 0) && (bit % 32) == 0) {
-				buffer[i++] = ' ';
+
+			buffer[index++] = ' ';// Put a space every byte
+			if ((i != 0) && (i % (sizeof(u128) / 2)) == 0) {// Put an extra space half way through
+				buffer[index++] = ' ';
+			}
+			if ((i != 0) && (i % (sizeof(u128) / 4)) == 0) {// Put a space every 32 bits
+				buffer[index++] = ' ';
 			}
 		}
-		buffer[i++] = ' ';
-		buffer[i++] = ' ';
-		buffer[i++] = ' ';
-		for (int bit = sizeof(u64) * 8 - 1; bit >= 0; bit--) {
-			buffer[i++] = DIGITS[(value.low >> bit) & 0b1];
-			if ((bit != 0) && (bit % 8) == 0) {
-				buffer[i++] = ' ';
-			}
-			if ((bit != 0) && (bit % 32) == 0) {
-				buffer[i++] = ' ';
-			}
-		}
-		assert(i == sizeof(buffer));
+		assert(index == sizeof(buffer));
 		os.write(buffer, sizeof(buffer));
-		
 	}
 	return os;
 }
+
+
+
+}//namespace
