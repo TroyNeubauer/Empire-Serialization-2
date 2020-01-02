2c85c6610568ea8851753b2cfdffc7774bfd02aa
Troy Neubauer <troyneubauer@gmail.com>
2020-01-02 03:44:20 -0500

    Added esc4 test

--- Test/src/ConversionsTest.h
+++ Test/src/ConversionsTest.h
@@ -1,9 +1,25 @@
 
 #include <catch2/catch.hpp>
 
+#include <EmpireSerialization2.h>
+#include <EmpireSerialization/Conversions.h>
 
-TEST_CASE("")
+using namespace ES;
+
+TEST_CASE("utf8->esc4 convert")
 {
+	char in[] = "test";
+	esc4 dest[100];
+	StringCodingData data;
+	ErrorCode error = Conversions::Convert<utf8, esc4>(in, std::strlen(in), dest, sizeof(dest), data);
+	REQUIRE(error == ErrorCode::NONE);
+	if (error) {
+		DefaultFormatter formatter;
+		ToString::PrintError(formatter, ES::GetError());
+		printf("Error converting string %s", formatter.c_str());
+	}
+	REQUIRE(data.Bytes == 2);
+	REQUIRE(data.Characters == 4);
 
 
 }
